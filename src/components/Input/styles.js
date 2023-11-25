8bb347e44b6c39020b0facd31fd9561d624e2613
Viviane-Maria <viviane.m.oliveira.s@gmail.com>
2023-11-25 17:43:29 -0300

    add percentage button

---
+++ src/components/Input/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        padding: 0 10px;
+        text-align: right;
+        font-size: 24px;
+        font-weight: 700;
+        color: #FFFFFF;
+    }
+`
